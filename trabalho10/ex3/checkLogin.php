eb972f7016c27e5a4d3fb67a532770e1500ce814
IgorAugust0 <augustoigor269@gmail.com>
2023-05-19 00:01:35 -0300

    teste ex3

--- trabalho10/ex3/checkLogin.php
+++ trabalho10/ex3/checkLogin.php
@@ -1,25 +1,39 @@
 <?php
 
-class RequestResponse
-{
-  public $success;
-  public $detail;
+// class RequestResponse
+// {
+//   public $success;
+//   public $detail;
 
-  function __construct($success, $detail)
-  {
-    $this->success = $success;
-    $this->detail = $detail;
-  }
-}
+//   function __construct($success, $detail)
+//   {
+//     $this->success = $success;
+//     $this->detail = $detail;
+//   }
+// }
+
 
 $email = $_POST['email'] ?? '';
 $senha = $_POST['senha'] ?? '';
 
 // Validação simplificada para fins didáticos. Não faça isso!
-if ($email == 'teste@mail.com' && $senha == '123456')
-  $response = new RequestResponse(true, 'home.html');
-else
-  $response = new RequestResponse(false, '');
+// if ($email == 'teste@mail.com' && $senha == '123456')
+//   $response = new RequestResponse(true, 'home.html');
+// else
+//   $response = new RequestResponse(false, '');
+
+
+if ($email === 'test@mail.com' && $senha === '123456') {
+  $response = array(
+    'success' => true,
+    'detail' => 'home.html'
+  );
+} else {
+  $response = array(
+    'success' => false,
+    'detail' => ''
+  );
+}
 
 header('Content-type: application/json; charset=utf-8');
 echo json_encode($response);
