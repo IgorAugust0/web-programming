b783f09ac234aef2645f536b83b3224c61dc408c
IgorAugust0 <augustoigor269@gmail.com>
2023-05-25 23:55:39 -0300

    trab11 ex2

--- trabalho11/ex2/busca-endereco.php
+++ trabalho11/ex2/busca-endereco.php
@@ -16,12 +16,5 @@ class Endereco
 
 $cep = $_GET['cep'] ?? '';
 
-if ($cep == '38400-100')
-  $endereco = new Endereco('Av Floriano', 'Centro', 'Uberlândia');
-else if ($cep == '38400-200')
-  $endereco = new Endereco('Rua Tiradentes', 'Fundinho', 'Uberlândia');
-else
-  $endereco = new Endereco('', '', '');
-
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($endereco);
