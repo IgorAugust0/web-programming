04e7ec02486a24922fdf3f8358ac4f87266d935b
IgorAugust0 <augustoigor269@gmail.com>
2023-06-01 10:59:04 -0300

    add api fecth with async/await and fixes

--- trabalho12/demo/src/main/java/com/example/demo/AddressController.java
+++ trabalho12/demo/src/main/java/com/example/demo/AddressController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -45,7 +46,7 @@ public class AddressController {
     }
 
     @PostMapping("/address")
-    public ResponseEntity<Address> createAddress(Address address) {
+    public ResponseEntity<Address> createAddress(@RequestBody Address address) {
         this.addresses.add(address);
         return new ResponseEntity<Address>(address, HttpStatus.CREATED);
     }
